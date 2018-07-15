b1be07eeef9d88a6a2ebf8b5aaaa01334081b188
p4radime <40726344+p4radime@users.noreply.github.com>
2018-07-15 22:52:15 +1000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,19 @@
+# The program says hello and asks for my name
+
+print ('helo world!')
+
+print ('what is your name?')    #ask for their name
+
+my_name = input()
+
+print ('it is good to meet you, ' + my_name)
+
+print ('The lengthof your name is:')
+
+print (len(my_name))
+
+print ('what is your age?') #ask for their age
+
+my_age = input()
+
+print ('you will be ' + str(int(my_age) + 1) + 'in a year. ')
